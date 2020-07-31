6a9ddb55d0cdc9412edf589e0d10f86b52dd1938
“Rafael <rafachido12@hotmail.comgit config --global user.name Rafael Casillasgit config --global user.email rafachido12@hotmail.com>
2020-07-31 10:04:50 -0500

    N

--- twilio.js
+++ twilio.js
@@ -1,13 +1,5 @@
 module.exports = {
     serviceID: "VA5173c6648d2068a102959832b48e2f5f",
     accountSID: "ACb3c970625fcbf86fb6b65f5004db5f8c",
-    authToken: "c8dcea0f187595822b916c03f6d8a69f"
+    authToken: "d8f1f98c8cb3810e36e2e889f859d416"
 }
-
-
-// module.exports = {
-//     // serviceID: "IS9834279eb8a98856096d9ec0f5c9fc7c",
-//     serviceID: "VA8f19107c3db4054eea42d9701ec7d906",
-//     accountSID: "ACb8b03c02b47e0ac02164c3bfc034c313",
-//     authToken: "e18630f2c730b2d12b842d7cad2a6043"
-// }
