e01204d84c4bb68598a165b3b2a71162bfc06cc5
“Rafael <rafachido12@hotmail.comgit config --global user.name Rafael Casillasgit config --global user.email rafachido12@hotmail.com>
2020-08-24 00:33:47 -0500

    Repartidor

--- twilio.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    accountSID: "ACb3c970625fcbf86fb6b65f5004db5f8c",
-    authToken: "d8f1f98c8cb3810e36e2e889f859d416",
-    // serviceID: "VA5173c6648d2068a102959832b48e2f5f",
-    serviceID: "VA8f19107c3db4054eea42d9701ec7d906",
-    account: "ACb8b03c02b47e0ac02164c3bfc034c313",
-    token: "1a34d574105981b207d19d267e959364"
-}
